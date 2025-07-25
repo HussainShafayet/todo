50732be9c9d6ed099bd91e9be11600ee83dd7c64
shafayet <shafayetsec09@gmail.com>
2025-07-25 21:59:49 +0600

    app js update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 
 
-function App() {
-  return (
-    <div className="App">
-      {/* Minimal starter for todo app */}
-      <h1>Todo App</h1>
-      {/* Add your todo components here */}
-    </div>
-  );
-}
+
+const App = () => (
+  <div className="App">
+    {/* Minimal starter for todo app */}
+    <h1>Todo App</h1>
+    {/* Add your todo components here */}
+  </div>
+);
 
 export default App;
