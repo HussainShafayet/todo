c1e3ccf6e05b2f399352be7cfd3d8a299954f86e
shafayet <shafayetsec09@gmail.com>
2025-07-26 09:18:44 +0600

    seo optimize improve

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import Navbar from './Navbar';
-import Footer from './Footer';
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
-   <div className="flex flex-col min-h-screen bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-white transition-colors duration-300">
-  <Navbar />
-  <main className="flex-grow p-4">
-    {children}
-  </main>
-  <Footer />
-</div>
+    <div className="flex flex-col min-h-screen bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-white transition-colors duration-300">
+      <Navbar />
+      <main className="flex-grow p-4">
+        <h1 className="text-3xl sm:text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white mb-6 text-center">
+          Organize Your Tasks with{" "}
+          <span className="text-blue-600 dark:text-blue-400">Todo App</span>
+        </h1>
 
+        {children}
+      </main>
+      <Footer />
+    </div>
   );
 };
 
